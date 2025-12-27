5ea7089635ae2abb80404a2cdc43d20156878b5c
Brylle <bryanpalay119@gmail.com>
2025-12-27 17:17:47 +0800

    feat: add LoginPage component for user authentication

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,18 @@
+
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { LoginForm } from "./_components/Login-Form";
+
+
+export default async function LoginPage() {
+  const  session = await auth.api.getSession({
+    headers: await headers(),
+  })
+  if (session) {
+    return redirect("/");
+  }
+  return (
+    <LoginForm/>
+  );
+}
