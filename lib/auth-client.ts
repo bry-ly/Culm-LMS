d78cd0cdf2c65dbf0fefa433553f6b0d41352cfd
Brylle <bryanpalay119@gmail.com>
2025-12-27 17:22:41 +0800

    feat: add authClient for better authentication handling

---
+++ lib/auth-client.ts
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react";
+import { emailOTPClient, adminClient } from "better-auth/client/plugins";
+import { env } from "./env";
+
+export const authClient = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  plugins: [emailOTPClient(), adminClient()],
+});
