be1babcb8c38ed68ef27930a77aed66c504f32a9
Brylle <bryanpalay119@gmail.com>
2025-12-27 17:22:37 +0800

    feat: implement arcjet integration for enhanced security features

---
+++ lib/arcjet.ts
@@ -0,0 +1,18 @@
+import "server-only";
+
+import { env } from "./env";
+
+import arcjet, { detectBot, fixedWindow, protectSignup, sensitiveInfo, shield, slidingWindow } from "@arcjet/next";
+
+export { detectBot, fixedWindow, protectSignup, sensitiveInfo, shield, slidingWindow };
+
+export default arcjet({
+  key: env.ARCJET_KEY,
+  characteristics: ["fingerprint"],
+
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+  ],
+});
