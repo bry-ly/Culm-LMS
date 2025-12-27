f33054fbf7de62f594457b179b6f83870a1590f9
Brylle <bryanpalay119@gmail.com>
2025-12-27 17:19:24 +0800

    feat: implement CreateCourse function for admin course creation with validation and bot protection

---
+++ app/admin/courses/create/actions.ts
@@ -0,0 +1,76 @@
+"use server";
+
+import { requireAdmin } from "@/app/data/admin/require-admin";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import prisma from "@/lib/db";
+import { ApiResponse } from "@/lib/types";
+import { courseSchema, CourseSchemaType } from "@/lib/zodSchemas";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    }),
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5,
+    }),
+  );
+
+export async function CreateCourse(data: CourseSchemaType): Promise<ApiResponse> {
+  const session = await requireAdmin();
+
+  try {
+    const req = await request();
+    const decision = await aj.protect(req, {
+      fingerprint: session.user.id,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return {
+          status: "error",
+          message: "You have been block due to rate limiting",
+        };
+      } else {
+        return {
+          status: "error",
+          message: "You are a bot! if this a mistake contact our support",
+        };
+      }
+    }
+
+    const validation = courseSchema.safeParse(data);
+
+    if (!validation.success) {
+      return {
+        status: "error",
+        message: "Invalid Form Data",
+      };
+      // throw new Error("Something failed");
+    }
+
+    const course = await prisma.course.create({
+      data: {
+        ...validation.data,
+        userId: session?.user.id as string,
+      },
+    });
+
+    return {
+      status: "success",
+      message: "Course created successfully!",
+    };
+  } catch (error) {
+    console.error("Error creating course:", error);
+    return {
+      status: "error",
+      message: "Failed to create course",
+    };
+  }
+}
