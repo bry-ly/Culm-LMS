de09a437f7ebc788f33dd7f2378aac235fb229eb
Brylle <bryanpalay119@gmail.com>
2025-12-27 17:17:59 +0800

    feat: add LoginForm component for user authentication with social and email options

---
+++ app/(auth)/login/_components/Login-Form.tsx
@@ -0,0 +1,132 @@
+"use client";
+import { Google, Github } from "@aliimam/logos";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Send } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { useTransition, useState } from "react";
+import { toast } from "sonner";
+import { Spinner } from "@/components/ui/spinner";
+import { useRouter } from "next/navigation";
+
+export function LoginForm() {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [emailPending, startEmailTransition] = useTransition();
+  const [email, setEmail] = useState("");
+
+  async function signInWithGithub() {
+    startTransition(async () => {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed in with Github, you will be redirected... ");
+          },
+          onError: (error) => {
+            toast.error(error.error.message);
+          },
+        },
+      });
+    });
+  }
+
+  async function signInWithGoogle() {
+    startTransition(async () => {
+      await authClient.signIn.social({
+        provider: "google",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed in with Google, you will be redirected... ");
+          },
+          onError: (error) => {
+            toast.error(error.error.message);
+          },
+        },
+      });
+    });
+  }
+
+  async function signInWithEmail() {
+    startEmailTransition(async () => {
+      await authClient.emailOtp.sendVerificationOtp({
+        email: email,
+        type: "sign-in",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Email successfully sent");
+            router.push(`/verify-request?email=${email}`);
+          },
+          onError: () => {
+            toast.error("Error sending email"); 
+          },
+        },
+      });
+    });
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Welcome Back!</CardTitle>
+        <CardDescription>Login with your Github or Google account</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <Button onClick={signInWithGithub} className="w-full" variant="outline" disabled={isPending}>
+            {isPending ? (
+              <>
+                <Spinner className="mr-2" />
+                <span>Loading...</span>
+              </>
+            ) : (
+              <>
+                <Github className="size-4" />
+                <span>Sign in with Github</span>
+              </>
+            )}
+          </Button>
+          <Button onClick={signInWithGoogle} className="w-full" variant="outline" disabled={isPending}>
+            {isPending ? (
+              <>
+                <Spinner className="mr-2" />
+                <span>Loading...</span>
+              </>
+            ) : (
+              <>
+                <Google className="size-4" />
+                <span>Sign in with Google</span>
+              </>
+            )}
+          </Button>
+        </div>
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-card px-2 text-muted-foreground">Or Continue With</span>
+        </div>
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          </div>
+          <Button onClick={signInWithEmail} disabled={emailPending}>
+            {emailPending ? (
+              <>
+                <Spinner className="size-4" />
+                <span>Loading...</span>
+              </>
+            ) : (
+              <>
+                <Send className="size-4" />
+                <span>Continue with Email</span>
+              </>
+            )}
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
