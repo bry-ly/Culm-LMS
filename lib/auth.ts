3191e6b6ad1f9b740aa80684152e15b98f239d9b
Brylle <bryanpalay119@gmail.com>
2025-12-27 17:22:45 +0800

    feat: implement authentication module with betterAuth integration

---
+++ lib/auth.ts
@@ -0,0 +1,36 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { emailOTP, admin } from "better-auth/plugins";
+import prisma from "./db";
+import { env } from "./env";
+import { resend } from "./resend";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
+  
+  plugins: [
+    admin(),
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        await resend.emails.send({
+          from: "Fullbright College LMS <send@bryanpalay.me>",
+          to: [email],
+          subject: "FBC LMS - Verify your email!",
+          html: `<p>Your OTP is <strong>${otp}</strong></p>`,
+        });
+      },
+    }),
+  ],
+});
