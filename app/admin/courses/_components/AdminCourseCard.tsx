da8e47877f3c5b4821c951ed7f9a71ac4366579f
Brylle <bryanpalay119@gmail.com>
2025-12-29 14:32:21 +0800

    feat: add AdminCoursesCardSkeleton component for loading state representation in course cards

--- app/admin/courses/_components/AdminCourseCard.tsx
+++ app/admin/courses/_components/AdminCourseCard.tsx
@@ -2,6 +2,7 @@ import { AdminCourseType } from "@/app/data/admin/admin-get-courses";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useConstructUrl } from "@/hooks/use-constract-url";
 import { ArrowRight, Edit, Eye, MoreVertical, School, TimerIcon, Trash2Icon } from "lucide-react";
 import Image from "next/image";
@@ -26,20 +27,20 @@ export function AdminCourseCard({ data }: Course) {
           <DropdownMenuContent align="end" className="w-48">
             <DropdownMenuItem asChild>
               <Link href={`/admin/courses/${data.id}/edit`}>
-                <Edit className="size-4 mr-2"/>
+                <Edit className="size-4 mr-2" />
                 Edit Course
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem asChild>
               <Link href={`/courses/${data.slug}`}>
-                <Eye className="size-4 mr-2"/>
+                <Eye className="size-4 mr-2" />
                 Preview
               </Link>
             </DropdownMenuItem>
-            <DropdownMenuSeparator/>
+            <DropdownMenuSeparator />
             <DropdownMenuItem asChild>
-              <Link href={`admin/courses/${data.id}/delete`}>
-                <Trash2Icon className="size-4 mr-2 text-destructive"/>
+              <Link href={`/admin/courses/${data.id}/delete`}>
+                <Trash2Icon className="size-4 mr-2 text-destructive" />
                 Delete
               </Link>
             </DropdownMenuItem>
@@ -70,6 +71,36 @@ export function AdminCourseCard({ data }: Course) {
         >
           Edit Course <ArrowRight className="size-4" />
         </Link>
+      </CardContent>
+    </Card>
+  );
+}
+
+export function AdminCoursesCardSekeleton() {
+  return (
+    <Card className="group relative py-0 gap-0">
+      <div className="absolute top-2 right-2 z-10 flex items-center gap-2">
+        <Skeleton className="h-6 w-16 rounded-full" />
+
+        <Skeleton className="size-8 rounded-md" />
+      </div>
+      <div className="w-full relative h-fit">
+        <Skeleton className="w-full rounded-t-lg aspect-video h-62.5 object-cover " />
+      </div>
+      <CardContent className="p-4">
+        <Skeleton className="h-6 w-3/4 mb-2 rounded" />
+        <Skeleton className="h-4 w-full mb-4 rounded" />
+        <div className="mt-4 flex items-center gap-x-5">
+          <div className="flex items-center gap-x-2">
+            <Skeleton className="size-6 rounded-md" />
+            <Skeleton className="h-4 w-10 rounded " />
+          </div>
+          <div className="flex items-center gap-x-2">
+            <Skeleton className="size-6 rounded-md" />
+            <Skeleton className="h-4 w-10 rounded " />
+          </div>
+        </div>
+        <Skeleton className="mt-4 h-10 w-full rounded"/>
       </CardContent>
     </Card>
   );
