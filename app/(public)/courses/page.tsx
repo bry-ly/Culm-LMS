c88cdc5fb95aae421d8b9906903469724564ca39
Brylle <bryanpalay119@gmail.com>
2025-12-30 13:10:07 +0800

    feat: add PublicCoursesRoute component for displaying available courses

---
+++ app/(public)/courses/page.tsx
@@ -0,0 +1,39 @@
+import { getAllCourses } from "@/app/data/course/get-all-courses";
+import { PublicCourseCard, PublicCourseCardSkeleton } from "../_components/PublicCourseCard";
+import { Suspense } from "react";
+
+export default function PublicCoursesRoute() {
+  return (
+    <div className="mt-5">
+      <div className="flex flex-col space-y-2 mb-10">
+        <h1 className="text-3xl md:text-4xl tracking-tight font-bold">Explore Courses</h1>
+        <p className="text-muted-foreground max-w-lg">Browse our courses and start learning today.</p>
+      </div>
+      <Suspense fallback={<LoadingSkeletonLayout/>}>
+        <RenderCourses />
+      </Suspense>
+    </div>
+  );
+}
+
+async function RenderCourses() {
+  const courses = await getAllCourses();
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {courses.map((course) => (
+        <PublicCourseCard key={course.id} data={course} />
+      ))}
+    </div>
+  );
+}
+
+function LoadingSkeletonLayout() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {Array.from({ length: 9 }).map((_, index) => (
+        <PublicCourseCardSkeleton key={index} />
+      ))}
+    </div>
+  );
+}
