7d3e0cc407fe3d1ddcd66460ebeb09590ef29409
Brylle <bryanpalay119@gmail.com>
2026-01-02 11:05:44 +0800

    Remove DataTable component and associated data import from AdminIndexPage to streamline the admin interface, as the course-related data is no longer needed.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,6 @@
 import { SectionCards } from "@/components/sidebar/section-cards";
 import { ChartAreaInteractive } from "@/components/sidebar/chart-area-interactive";
-import { DataTable } from "@/components/sidebar/data-table";
 
-import data from "./data.json";
 
 export default function AdminIndexPage() {
   return (
@@ -11,7 +9,6 @@ export default function AdminIndexPage() {
       <div className="px-4 lg:px-6">
         <ChartAreaInteractive />
       </div>
-      <DataTable data={data} />
     </div>
   );
 }
