2045b2421f87c3c82dc4989a362420746976526e
Brylle <bryanpalay119@gmail.com>
2026-01-04 06:38:27 +0800

    Invoke form.reset() on modal close

--- app/admin/courses/[courseId]/edit/_components/NewChapterModal.tsx
+++ app/admin/courses/[courseId]/edit/_components/NewChapterModal.tsx
@@ -43,7 +43,7 @@ export function NewChapterModal({ courseId }: { courseId: string }) {
 
   function handleOpenChange(open: boolean) {
     if (!open) {
-      form.reset;
+      form.reset();
     }
 
     setIsOpen(open);
