6fdf6cc18e6c31cebed8ef6e19fd2a46580bf040
Brylle <bryanpalay119@gmail.com>
2026-01-04 06:41:31 +0800

    Add getEnrolledCourses and EnrolledCourseType

---
+++ app/data/user/get-enrolled-courses.ts
@@ -0,0 +1,55 @@
+import "server-only";
+import { requireUser } from "./require-user";
+import prisma from "@/lib/db";
+
+export async function getEnrolledCourses() {
+  const user = await requireUser();
+
+  const data = await prisma.enrollment.findMany({
+    where: {
+      userId: user.id,
+      status: "Active",
+    },
+    select: {
+      course: {
+        select: {
+          id: true,
+          smallDescription: true,
+          title: true,
+          filekey: true,
+          level: true,
+          duration: true,
+          slug: true,
+          category: true,
+          price: true,
+          chapter: {
+            select: {
+              id: true,
+              lesson: {
+                select: {
+                  id: true,
+                  lessonProgress: {
+                    where: {
+                      userId: user.id,
+                    },
+                    select: {
+                      completed: true,
+                      id: true,
+                      lessonId: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return data;
+}
+
+export type EnrolledCourseType = Awaited<
+  ReturnType<typeof getEnrolledCourses>
+>[0];
