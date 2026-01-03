2d216025dc33af8011cc845f090e9a2d5705dc30
Brylle <bryanpalay119@gmail.com>
2026-01-03 12:28:01 +0800

    Remove server-only import

--- app/data/admin/admin-get-dashboard-stats.ts
+++ app/data/admin/admin-get-dashboard-stats.ts
@@ -1,30 +1,29 @@
-import "server only";
-
 import prisma from "@/lib/db";
 import { requireAdmin } from "./require-admin";
 
 export async function adminGetDashboardStats() {
   await requireAdmin();
 
-  const [totalSignups, totalCustomers, totalCourses, totalLessons] = await Promise.all([
-    // user counts
-    prisma.user.count(),
+  const [totalSignups, totalCustomers, totalCourses, totalLessons] =
+    await Promise.all([
+      // user counts
+      prisma.user.count(),
 
-    //total customers
-    prisma.user.count({
-      where: {
-        enrollment: {
-          some: {},
+      //total customers
+      prisma.user.count({
+        where: {
+          enrollment: {
+            some: {},
+          },
         },
-      },
-    }),
+      }),
 
-    // Total courses
-    prisma.course.count(),
+      // Total courses
+      prisma.course.count(),
 
-    // Total lessons
-    prisma.lesson.count(),
-  ]);
+      // Total lessons
+      prisma.lesson.count(),
+    ]);
 
   return {
     totalCourses,
