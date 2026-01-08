f23daec4666b0bf8ae37719dcd725c6d41eb3c0e
Brylle <bryanpalay119@gmail.com>
2026-01-08 09:25:00 +0800

    Use Empty layout for 401 NotAuthorized page

--- app/not-admin/page.tsx
+++ app/not-admin/page.tsx
@@ -1,30 +1,41 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import { Compass, Home, ShieldAlert } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { ArrowLeft, ShieldX } from "lucide-react";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyTitle } from "@/components/ui/empty";
 
-export default function UnauthorizedError() {
-  const router = useRouter();
-
+export default function NotAuthorizedPage() {
   return (
-    <div className="flex h-svh items-center justify-center p-4">
-      <Card className="w-full max-w-md text-center shadow-lg">
-        <CardHeader className="flex flex-col items-center gap-2">
-          <div className="rounded-full bg-destructive/10 p-3">
-            <ShieldX className="h-10 w-10 text-destructive" />
-          </div>
-          <CardTitle className="text-3xl font-bold mt-1">Unauthorized Access</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-muted-foreground">You do not have permission to view this page. Please log in with the appropriate credentials or contact your administrator.</p>
-        </CardContent>
-        <CardFooter className="flex justify-center gap-4">
-          <Button onClick={() => router.push("/")}>
-            <ArrowLeft className="size-4" />Back to Home</Button>
-        </CardFooter>
-      </Card>
+    <div className="flex w-full items-center justify-center">
+      <div className="flex h-screen items-center border-x">
+        <div>
+          <div className="absolute inset-x-0 h-px bg-border" />
+          <Empty>
+            <EmptyHeader className="items-center">
+              <ShieldAlert className="mb-2 h-10 w-10 text-destructive" />
+              <EmptyTitle className="font-black font-mono text-6xl">401</EmptyTitle>
+              <EmptyDescription className="text-center">
+                You don&apos;t have permission to view this page.
+                <br />
+                Please return home or explore available content.
+              </EmptyDescription>
+            </EmptyHeader>
+            <EmptyContent>
+              <div className="flex gap-2">
+                <Button asChild>
+                  <a href="/">
+                    <Home /> Go Home
+                  </a>
+                </Button>
+                <Button asChild variant="outline">
+                  <a href="/courses">
+                    <Compass /> Explore
+                  </a>
+                </Button>
+              </div>
+            </EmptyContent>
+          </Empty>
+          <div className="absolute inset-x-0 h-px bg-border" />
+        </div>
+      </div>
     </div>
   );
 }
