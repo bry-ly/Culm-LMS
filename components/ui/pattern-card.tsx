5532f6e76851187a2c8b72d4a127a2ca8a7c6118
Brylle <bryanpalay119@gmail.com>
2026-01-09 11:01:36 +0800

    Add PatternCard UI component

---
+++ components/ui/pattern-card.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface PatternCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+}
+
+import { CornerBorders } from "@/components/ui/corner-borders";
+
+export function PatternCard({
+  className,
+  children,
+  ...props
+}: PatternCardProps) {
+  return (
+    <div
+      className={cn("bg-pattern-striped p-6 relative", className)}
+      {...props}
+    >
+      <CornerBorders />
+      {children}
+    </div>
+  );
+}
