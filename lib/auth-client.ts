f2997b3ae3cb170d6fa23254ed23b6956907ded7
Brylle <bryanpalay119@gmail.com>
2026-01-29 07:22:38 +0800

    feat(auth-client): add lastLoginMethod plugin support

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,12 @@
 import { createAuthClient } from "better-auth/react";
-import { emailOTPClient, adminClient } from "better-auth/client/plugins";
+import {
+  emailOTPClient,
+  adminClient,
+  lastLoginMethodClient,
+} from "better-auth/client/plugins";
 import { env } from "./env";
 
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_BETTER_AUTH_URL,
-  plugins: [emailOTPClient(), adminClient()],
+  plugins: [emailOTPClient(), adminClient(), lastLoginMethodClient()],
 });
