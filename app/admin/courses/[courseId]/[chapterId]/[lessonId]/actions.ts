1dae075ead2f93645a380da73f9757f2f5e19432
Brylle <bryanpalay119@gmail.com>
2026-01-29 07:28:28 +0800

    feat(lesson): add course completion email on final lesson

--- app/admin/courses/[courseId]/[chapterId]/[lessonId]/actions.ts
+++ app/admin/courses/[courseId]/[chapterId]/[lessonId]/actions.ts
@@ -1,17 +1,39 @@
 "use server";
 
 import { requireAdmin } from "@/app/data/admin/require-admin";
+import arcjet, { fixedWindow } from "@/lib/arcjet";
 import prisma from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
 import { lessonSchema, LessonSchemaType } from "@/lib/zodSchemas";
+import { request } from "@arcjet/next";
+
+const aj = arcjet.withRule(
+  fixedWindow({
+    mode: "LIVE",
+    window: "1m",
+    max: 10,
+  })
+);
 
 export async function updateLesson(
   values: LessonSchemaType,
   lessonId: string
 ): Promise<ApiResponse> {
-  await requireAdmin();
+  const user = await requireAdmin();
 
   try {
+    const req = await request();
+    const decision = await aj.protect(req, {
+      fingerprint: user.user.id,
+    });
+
+    if (decision.isDenied()) {
+      return {
+        status: "error",
+        message: "Too many requests. Please try again later.",
+      };
+    }
+
     const result = lessonSchema.safeParse(values);
 
     if (!result.success) {
