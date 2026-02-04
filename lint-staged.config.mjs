ce06484fb2c36d2f765927d652586e9bc7489b55
Brylle <bryanpalay119@gmail.com>
2026-02-05 00:47:16 +0800

    chore(repo): use prisma format for schema

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 const lintStagedConfig = {
   "*.{js,jsx,ts,tsx,mjs}": ["eslint --fix", "prettier --write"],
   "*.{json,md,mdx,css,html,yml,yaml}": ["prettier --write"],
-  "*.prisma": ["prettier --write"],
+  "*.prisma": () => "npx prisma format --schema prisma/schema.prisma",
 };
 
 export default lintStagedConfig;
